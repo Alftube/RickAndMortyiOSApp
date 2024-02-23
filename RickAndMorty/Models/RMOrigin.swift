7bbf30630ca5efc6130d69b9c466705dcb334e41
Alf <alfredoaltube@gmail.com>
2024-02-23 18:26:31 -0300

    added models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Alfredo Altube on 23/02/2024.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,14 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Alfredo Altube on 23/02/2024.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
+
